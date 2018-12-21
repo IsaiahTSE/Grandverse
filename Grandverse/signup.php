8bcddab96ff639a6b36bc210ed33d22a40444a8b
Isaiah Contreras <36010290+IsaiahTSE@users.noreply.github.com>
2018-12-20 21:42:26 -0500

    Update signup.php

--- Grandverse/signup.php
+++ Grandverse/signup.php
@@ -22,7 +22,7 @@ if(empty($_SESSION['signed_in'])){
 
                 <div class="auth-input-double">               
                     <label>
-                        <input type="text" name="username" maxlength="16" title="Cedar ID" placeholder="User ID" value="">
+                        <input type="text" name="username" maxlength="16" title="Account ID" placeholder="User ID" value="">
                     </label>
                     <label>
                         <input type="password" name="password" maxlength="16" title="Password" placeholder="Password">
